225ea583660288d9cc2c93ea83d079468e9ca8b1
Noeingala <ingalanoelia@gmail.com>
2023-10-24 18:10:31 -0300

    menu hamburguesa listo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,17 @@ import style from './Navbar.module.css';
 import {getImageUrl} from '../utils';
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={style.navbar}>
         <a className={style.title} href="/">Portfolio</a>
         <div className={style.menu}>
-          <img className={style.menuBtn} src={getImageUrl('Nav/icons8-menú-50.png')} alt='menu-button'/>
-            <ul className={style.menuItems}>
+          <img className={style.menuBtn} 
+                src={menuOpen ? getImageUrl('Nav/icons8-menú-50.png') : getImageUrl('Nav/icons8-x-48.png')}  
+                alt='menu-button' 
+                onClick={()=>setMenuOpen(!menuOpen)}/>
+            <ul className={`${style.menuItems} ${menuOpen && style.menuOpen}`}>
                 <li><a href='#about'>About</a></li>
                 <li><a href='#habilidades'>Habilidades</a></li>
                 <li><a href='#proyectos'>Proyectos</a></li>
